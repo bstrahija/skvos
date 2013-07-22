d8d9f95fedd8c044ffe91b1d8ba1d0183ee86b3f
Boris Strahija <boris@creolab.hr>
2013-07-22 10:09:38 +0200

    Different button color

--- app/views/dashboard/_partial/event_people.blade.php
+++ app/views/dashboard/_partial/event_people.blade.php
@@ -83,7 +83,7 @@
 				<a href="{{ url('invitations/resend', $event->id) }}" class="button button-circle button-royal pull-right" title="Pošalji svima"><i class="icon-envelope-alt"></i></a>
 				<a href="{{ url('invitations/send', $event->id) }}" class="button button-circle button-primary pull-left" title="Pošalji svima kojima nije poslano"><i class="icon-envelope"></i></a>
 
-				<a href="{{ route('events.edit', $event->id) }}" class="button button-circle button-caution"><i class="icon-pencil"></i></a>
+				<a href="{{ route('events.edit', $event->id) }}" class="button button-circle button-highlight"><i class="icon-pencil"></i></a>
 			</div>
 		@endif
 	@endif
