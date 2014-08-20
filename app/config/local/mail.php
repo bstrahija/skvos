0ceac01e889d7382dd21625a995c9abd72177023
Boris Strahija <bstrahija@gmail.com>
2014-08-20 22:19:21 +0200

    Better config

--- app/config/local/mail.php
+++ app/config/local/mail.php
@@ -5,11 +5,7 @@ return array(
 	'pretend' => false,
 	'comment_notification_delay' => 1,
 
-	'host'       => 'smtp.mandrillapp.com',
-	'port'       => 2525,
-	'from'       => array('address' => 'info@skvosh.in', 'name' => 'Skvosh.in'),
-	'encryption' => 'tls',
-
+	'from'      => array('address' => 'info@skvosh.in', 'name' => 'Skvosh.in'),
 	'whitelist' => array('bstrahija@gmail.com'),
 	'blacklist' => array(),
 
