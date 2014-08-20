118b5b90ab83c563ec5af0a00ee70db94fdbb0fe
Boris Strahija <bstrahija@gmail.com>
2014-08-20 22:23:55 +0200

    Add production specific config

---
+++ app/config/production/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'pretend' => false,
+
+	'host'       => 'smtp.mandrillapp.com',
+	'port'       => 587,
+	'from'       => array('address' => 'info@skvosh.in', 'name' => 'Skvosh.in'),
+	'encryption' => 'tls',
+
+);
